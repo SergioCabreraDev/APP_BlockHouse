f5bb42bc316a61fc1e17a4e142d2891cc47d3ee0
SergioCabreraDev <scabrera.main@gmail.com>
2023-09-14 11:26:49 +0200

    commit

--- app.js
+++ app.js
@@ -5,3 +5,34 @@ const nav    = document.querySelector('.nav')
 button.addEventListener('click',()=>{
     nav.classList.toggle('activo')
 })
+
+
+// Cargar el archivo de traducciones
+$.getJSON('/lang/lang.json', function(translations) {
+    // Establecer el idioma predeterminado (español)
+    var currentLang = 'es';
+  
+    // Función para cambiar el idioma
+    function changeLanguage(lang) {
+      $('[data-lang]').each(function() {
+        var key = $(this).data('lang');
+        if (translations[lang] && translations[lang][key]) {
+          $(this).text(translations[lang][key]);
+        }
+      });
+    }
+  
+    // Cambiar el idioma al cargar la página
+    changeLanguage(currentLang);
+  
+    // Manejar el clic en los botones de cambio de idioma
+    $('.translate').click(function() {
+      var lang = $(this).attr('id');
+      if (lang !== currentLang) {
+        changeLanguage(lang);
+        currentLang = lang;
+      }
+    });
+  });
+  
+
